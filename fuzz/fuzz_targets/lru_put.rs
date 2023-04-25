78158bac284f154ecf5cc5fb8efa59447b6b2ff4
Matthew Jurenka <matt.jurenka@comcast.net>
2023-04-25 22:20:37 +0000

    initialize lru_put fuzz

---
+++ fuzz/fuzz_targets/lru_put.rs
@@ -0,0 +1,11 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use lru::LruCache;
+use std::num::NonZeroUsize;
+
+// TODO: add docs about arbitrary
+fuzz_target!(|value: (u8, &[u8])| {
+    let (key, val) = value;
+    let mut cache = LruCache::new(NonZeroUsize::new(1).unwrap());
+    cache.put(key, val);
+});
